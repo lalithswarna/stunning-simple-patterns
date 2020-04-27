70d01529eed3c8e6420b348c533e173cfd51deb3
LalithAIS <lalith.swarna@gmail.com>
2020-04-27 23:45:38 +0530

    Bridge Pattern

---
+++ Pattern5Bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pattern5Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
